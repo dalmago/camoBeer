03009d33f181c8f91e1631d73c465359812622f6
Matheus Dal Mago <matheusdalmago10@hotmail.com>
2016-09-22 22:13:51 -0300

    adding logo images

--- Server/camobeer/beerCatalog/models.py
+++ Server/camobeer/beerCatalog/models.py
@@ -3,12 +3,14 @@ from django.db import models
 # Create your models here.
 class BeerBrand(models.Model):
     brand_name = models.CharField(max_length=30)
+    brand_logo = models.ImageField(upload_to="brands")
 
     def __str__(self):
         return self.brand_name
 
 class Vendor(models.Model):
     vendor_name = models.CharField(max_length=30)
+    vendor_logo = models.ImageField(upload_to="vendors")
 
     def __str__(self):
         return self.vendor_name
