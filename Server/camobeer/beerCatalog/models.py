67ed6070533c4ed8d1ab803bd3fe811b3ace60ee
Matheus Dal Mago <matheusdalmago10@hotmail.com>
2016-10-03 15:52:26 -0400

    Server - adding optional fields

--- Server/camobeer/beerCatalog/models.py
+++ Server/camobeer/beerCatalog/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class BeerBrand(models.Model):
     brand_name = models.CharField(max_length=30)
-    brand_logo = models.ImageField(upload_to="brands", blank=True)
+    brand_logo = models.ImageField(upload_to="brands", null=True, blank=True)
 
     def __str__(self):
         return self.brand_name
@@ -13,7 +13,7 @@ class BeerBrand(models.Model):
 
 class Vendor(models.Model):
     vendor_name = models.CharField(max_length=30)
-    vendor_logo = models.ImageField(upload_to="vendors", blank=True)
+    vendor_logo = models.ImageField(upload_to="vendors", null=True, blank=True)
 
     def __str__(self):
         return self.vendor_name
@@ -43,7 +43,7 @@ class Beer(models.Model):
     beer_type = models.CharField(max_length=1, choices=BEER_SIZE)
     vendor = models.ForeignKey(Vendor)
     price = models.FloatField()
-    note = models.CharField(max_length=15, default="", blank=True)
+    note = models.CharField(max_length=15, default="", null=True, blank=True)
 
     def __str__(self):
         return ("%s %s %s" % (self.brand, self.get_beer_type_display(), self.vendor))
